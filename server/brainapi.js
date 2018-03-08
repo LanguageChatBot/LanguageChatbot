31690c4b31577424988480f4390ada625fa1f4f4
Kathia Rieser <kath.medulla@gmail.com>
2018-03-08 15:18:51 +0100

    solve mistakes

--- server/brainapi.js
+++ server/brainapi.js
@@ -1,11 +1,11 @@
 const Frases = require("./models/Brain");
 const User = require("./models/User");
 let index = 0;
-//let i = 0;
+let i = 0;
 
 module.exports = (word, id) => {
   return new Promise((resolve, reject) => {
-    if (word.split(" ")[0] == "ris") {
+    if (word.split(" ")[0] == "ris" && word.split(" ")[1]!= "marc") {
       Frases.findOne({ tag: "game" }).then(function(res) {
         if (res.reply[index].tag == word.split(" ")[1]) {
           resolve('Yes!🔝 you get it! Write play for keep gaming! 😉');
@@ -27,6 +27,31 @@ module.exports = (word, id) => {
         }
       });
     }
+
+    if (word.split(" ")[0] == "ris" && word.split(" ")[1]== "marc") {
+      Frases.findOne({ tag: "ironhack" }).then(function(res) {
+        if (res.reply[index].tag == word.split(" ")[1]) {
+          resolve('Yaiza!! you are soooo GOOOOD TODAY! 😉');
+          index++;
+          if (index >= res.reply.length) {
+            index = 0;
+          }
+          User.findByIdAndUpdate(
+            id,
+            { $inc: { progress: 10 } },
+            { new: true },
+            function(err, user) {
+              if (err) return handleError(err);
+              // res.send(user);
+            }
+          );
+        } else {
+          resolve('Adrian Shut UP!!');
+        }
+      });
+    }
+
+
     if (word == "play") {
       Frases.findOne({ tag: "game" }).then(function(res) {
         let result = res.reply[index].value;
@@ -49,8 +74,10 @@ module.exports = (word, id) => {
         });
       });
     }
-    
-    Frases.findOne({ "answer.value": word })
+
+    if (word.charAt(word.length - 1) != "?" && word.split(" ")[0] != "ris" && word != 'ironhack' && word != "play"){
+
+      Frases.findOne({ "answer.value": word })
       .then(function(res) {
         Frases.findOne({ tag: res.tag }).then(function(res) {
           let result =
@@ -63,5 +90,12 @@ module.exports = (word, id) => {
           "Your sentence seems to be correct, but I don't understand what you meen"
         )
       ); //No Tag found
+
+
+  }
+    
+   
+
+
   });
 };
